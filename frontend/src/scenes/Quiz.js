05bb90ab0d03fc9279d092bc52abd32b1de40cfe
Matthew O'Boyle <oboyle.matthew@duke.edu>
2018-11-15 19:18:21 -0500

    Mapping

--- frontend/src/scenes/Quiz.js
+++ frontend/src/scenes/Quiz.js
@@ -6,6 +6,12 @@ export default class Quiz extends Component {
             <div className="App">
                 <p>This is the quiz</p>
                 <p>Here is the text from Home: {this.props.app.text}</p>
+                {this.props.app.array.map(obj => (
+                    <div>
+                        <h5>{obj.question}</h5>
+                        <p>{obj.answer}</p>
+                    </div>
+                ))}
             </div>
         );
     }
