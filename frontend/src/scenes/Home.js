80192d513a440508e93a527ddbe341875610193a
Nico Bodkin <nb158@duke.edu>
2018-11-15 19:53:03 -0500

    some front

--- frontend/src/scenes/Home.js
+++ frontend/src/scenes/Home.js
@@ -13,10 +13,12 @@ export default class Home extends Component {
     render() {
         return (
             <div className="App">
-                <h1>{this.props.title}</h1>
+                <br/>
+                <p>Welcome to Homework Helper! Paste your notes below :)</p>
                 <textarea style={{width:"50vw", height:"40vh"}} value={this.state.text} onChange={e => this.setState({text: e.target.value})}/>
                 <br/>
                 <button onClick={() => this.props.app.text = this.state.text}>Submit</button>
+
             </div>
         );
     }
